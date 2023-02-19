edefd569431d6489058859bade19ef5b6e03aed0
Seth R Johnson <johnsonsr@ornl.gov>
2023-02-19 13:20:28 -0500

    Update scripts

---
+++ nb/admin/github-stars.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+# See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+"""
+"""
+
+from itertools import chain
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ghapi.all import GhApi, paged, gh2date
+
+with open('/Users/seth/.config/ghapi-token') as f:
+    token = f.read().strip()
+
+celer = GhApi(owner='celeritas-project', repo='celeritas', token=token)
+
+def get_stars(repo):
+    star_props = chain.from_iterable(paged(
+        repo.activity.list_stargazers_for_repo,
+        headers=dict(accept='application/vnd.github.star+json')
+    ))
+    stardate = sorted([gh2date(p['starred_at']) for p in star_props])
+    stars = pd.Series(np.arange(len(stardate), dtype=int),
+                  index=pd.DatetimeIndex(stardate))
+    return stars
+
+
+celer_stars = get_stars(celer)
+# adept_stars = get_stars(GhApi(owner='apt-sim', repo='AdePT', token=token))
+#stars = pd.DataFrame({'celeritas': celer_stars, 'adept': adept_stars})
+stars = celer_stars
+
+(fig, ax) = plt.subplots(figsize=(3,3))
+stars.plot(ylabel='# stars', ax=ax);
+ax.grid(which='major', c=(0.85,)*3)
+fig.savefig('github-stars.pdf', transparent=True)
---
+++ nb/admin/run-cloc.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -ex
+# Copyright 2023 UT-Battelle, LLC, and other Celeritas developers.
+# See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+ADMINDIR=$(cd $(dirname $0) && pwd)
+
+if ! hash cloc ; then
+  echo "This script requires https://github.com/AlDanial/cloc"
+  exit 1
+fi
+
+cd /Users/seth/.local/src/celeritas-temp
+VERSION=$(git describe --tags --match='v*')
+
+OUTDIR=${ADMINDIR}/run-cloc/${VERSION}
+mkdir $OUTDIR || true
+
+function run_cloc() {
+  cloc --git HEAD --force-lang=CUDA,hip --csv --quiet
+}
+
+run_cloc --exclude-dir=app,generated,scripts,test > $OUTDIR/library.csv
+run_cloc --match-d='generated' > $OUTDIR/generated.csv
+run_cloc --fullpath --match-d='/app/' > $OUTDIR/app.csv
+run_cloc --fullpath --match-d='/test/' > $OUTDIR/test.csv
+run_cloc --fullpath --match-d='/example/' > $OUTDIR/example.csv
+
